3601f0ff6ca9d55d54cd898c09abc7814c3c1d9b
solomonadzape95 <adzapesolomon@gmail.com>
2025-10-09 14:58:02 +0100

    feat: added schedule page for hackers

---
+++ components/hackers/schedule.tsx
@@ -0,0 +1,111 @@
+
+import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
+import Image from "next/image";
+
+const journeyImages = [
+  {
+    text: (
+      <>
+        Online Hackathon officially starts with workshops from sponsors.
+      </>
+    ),
+    date: "Mon, 28th Oct, 2024",
+    gridSpan: "md:col-span-3 bg-[#C89A04]",
+  },
+  {
+    text: (
+      <>
+        Online Hackathon day(2) continues with the hackers and their teams.
+      </>
+    ),
+    date: "Tue, 29th Oct, 2024",
+    gridSpan: "md:col-span-3 bg-[#51604D]",
+  },
+  {
+    text: (
+      <>
+        Admissions to all registered participants to the hacker house for the physical phase.
+      </>
+    ),
+    date: "Wed, 30th Oct, 2024",
+    gridSpan: "md:col-span-3 bg-[#024539]",
+  },
+  {
+    text: (
+      <>
+        Physical hackathon day(2) continues with hackers rounding up.
+      </>
+    ),
+    date: "Thu, 31st Oct, 2024",
+    gridSpan: "md:col-span-3 bg-[#C89A04]",
+  },
+  {
+    text: (
+      <>
+        Judges will review and judge, and the top 10 projects get to do a physical presentation.
+      </>
+    ),
+    date: "Fri, 1st Nov, 2024",
+    gridSpan: "md:col-span-3 bg-[#51604D]",
+  },
+  {
+    text: (
+      <>
+        Conference 4.0 grand finale event for all techies. (Newbies, Designers, Developers, Content writers, etc.)
+      </>
+    ),
+    date: "Sat, 2nd Nov, 2024",
+    gridSpan: "md:col-span-3 bg-[#024539]",
+  },
+]
+
+export default function HackerSchedule() {
+  return (
+    <div
+      className="bg-[url('/assets/events/hero_bg.jpg')] bg-cover bg-center w-full min-h-[400px] max-h-[60vh] pt-16 space-y-12"
+      style={{ height: "auto" }}
+    >
+      <section className="w-2/5 mx-auto flex items-center justify-center gap-3">
+        <div className="w-full md:max-w-md">
+          <Image
+            src="/assets/events/buildathon.png"
+            alt="Buildathon Hero"
+            width={240}
+            height={135}
+            className="object-contain"
+          />
+        </div>
+        <p
+          className="text-6xl text-[#008b37] font-extrabold relative inline-block"
+          style={{
+            textShadow:
+              "-4px 4px 0 #014736, -8px 8px 0 #014736, 0px 2px 12px rgba(0,0,0,0.25)",
+          }}
+        >
+          Schedule
+        </p>
+      </section>
+      <section className="grid grid-cols-1 md:grid-rows-3 md:grid-cols-6 w-full lg:max-h-[50vh] mt-auto">
+        {journeyImages.map((item, index) => (
+          <div
+            key={index}
+            className={`${item.gridSpan} relative p-3 text-center w-full h-[18vh] border-2 border-black border-collapse flex items-center justify-center text-3xl group overflow-hidden`}
+          >
+            <span
+              className={cn(
+                "border-3 border-black w-3/4 h-10 absolute top-3 text-2xl text-center bg-[#00cd51] flex items-center justify-center rounded left-1/2 -translate-x-1/2"
+               
+              )}
+            >
+              DAY {index + 1}: {item.date.toUpperCase()}
+            </span>
+            <div className="w-full flex flex-col items-center justify-center text-2xl mt-8 p-3">
+              {item.text}
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+}
